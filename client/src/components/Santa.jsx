d45f1b08838e3b5548a17ce55aec0e10a63bf5ae
Tarallo <tarantinofranc3sco@gmail.com>
2023-11-29 15:00:49 +0100

    .

--- client/src/components/Santa.jsx
+++ client/src/components/Santa.jsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react';
 import confetti from "https://esm.run/canvas-confetti@1";
+import { faUser, faAt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Santa() {
   const [showSanta, setShowSanta] = useState(false);
   const [username, setUsername] = useState("");
+  const [organizerEmail, setOrganizerEmail] = useState('');
   const [temporaryUsername, setTemporaryUsername] = useState("");
   const [participants, setParticipants] = useState([]);
   const [newParticipantName, setNewParticipantName] = useState('');
+  const [participantEmail, setParticipantEmail] = useState('');
   const [numParticipants, setNumParticipants] = useState(0);
   const [groupName, setGroupName] = useState("Your Group's name");
   const [assignments, setAssignments] = useState([]);
@@ -18,72 +22,118 @@ export default function Santa() {
     if(!showSanta) setShowSanta(true)
   }
 
+  // Function to send participant data to the server in order to send the e-mail
+  const sendParticipantData = async () => {
+    try {
+      const allParticipants = [...participants];
+
+      // Make a POST request to server
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ participants: allParticipants, organizer: username, date: timestamp }),
+      });
+
+      if (response.ok) {
+        setMessage("🎁 Secret Santas assigned and email sent to each participant!");
+
+        setTimeout(() => {
+          setMessage("");
+        }, 5000);
+
+        confetti({
+          particleCount: 150,
+          spread: 60
+        });
+      } else {
+        setMessage("❌ Error, please try again.");
+
+        setTimeout(() => {
+          setMessage("");
+        }, 5000);
+      }
+    } catch (error) {
+      console.error('Error sending participant data:', error);
+    }
+  };
+
   // Add Participant to the santa group
   const handleAddParticipant = () => {
       if (newParticipantName.trim() !== '') {
         const randomEmoji = getRandomEmoji();
-        setParticipants([...participants, { name: newParticipantName, emoji: randomEmoji }]);
+        setParticipants([...participants, { 
+          name: newParticipantName, 
+          email: participantEmail, 
+          emoji: randomEmoji }]);
         setNewParticipantName('');
+        setParticipantEmail('');
         setNumParticipants(numParticipants + 1);
       }
-    }
+  }
+
   // Generate random emoji
   const getRandomEmoji = () => {
     const emojis = ["🎁", "🎅", "❄️", "☃️", "🎄", "🕯️", "⛄", "🌟", "🦌", "🤶", "🧑‍🎄", "👼"];
     return emojis[Math.floor(Math.random() * emojis.length)];
   }
+  const handleSanta = () => {
+    if (numParticipants >= 2) {
+      const santasAssigned = assignSantas([...participants]);  
+      setAssignments(santasAssigned);
+      // Send participant function to send to call the backened
+      sendParticipantData();
+    } else{
+      setMessage('❌ Number of participants must be at least two for Secret Santa!');
+      setTimeout(() => {
+        setMessage("");
+      }, 5000);
+    } 
+  };
 
-// On button click, handle santa
-const handleSanta = () => {
-  if (numParticipants % 2 === 0 && numParticipants >= 2) {
-    setMessage("🎁 Secret Santas assigned!");
-    setTimeout(() => {
-      setMessage("");
-    }, 5000)
-    const santasAssigned = assignSantas([...participants]);
-    setAssignments(santasAssigned);
-    confetti({
-      particleCount: 150,
-      spread: 60
-   });
-  } else if(numParticipants < 2) {
-    setMessage('❌ Number of participants must be at least two for Secret Santa!');
-    setTimeout(() => {
-      setMessage("");
-    }, 5000)
-  } else {
-    setMessage('❌ Number of participants must be an even number for Secret Santa!');
-    setTimeout(() => {
-      setMessage("");
-    }, 5000)
-  }
-};
-// Handle user's name 
+// Handle user's name and email (organizer)
 const handleSubmitUsername = () => {
-    if (temporaryUsername.trim() !== '') {
+  if (temporaryUsername.trim() !== '') {
       const currentDate = new Date();
       const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
       const formattedTimestamp = currentDate.toLocaleDateString('en-US', options);
       setTimestamp(formattedTimestamp);
-      setUsername(temporaryUsername);
-      setTemporaryUsername('');
-      setNumParticipants(numParticipants + 1); 
-      setParticipants([...participants, { name: temporaryUsername, emoji: getRandomEmoji() }]);
-    }
+
+      // Check if the organizer is already in the list
+      const organizerExists = participants.some(participant => participant.name === temporaryUsername);
+      if (!organizerExists) {
+          setUsername(temporaryUsername);
+          setTemporaryUsername('');
+          setNumParticipants(numParticipants + 1);
+
+          // Add the organizer only if they don't exist in the list
+          setParticipants(prevParticipants => [
+              ...prevParticipants,
+              {
+                  name: temporaryUsername,
+                  email: organizerEmail,
+                  emoji: getRandomEmoji()
+              }
+          ]);
+      }
+   }
 }
+
 // Assigning Santas with partecipants as parameter 
-  const assignSantas = (participants) => {
-    // Shuffle the participants array randomly
-    const shuffledParticipants = participants.sort(() => Math.random() - 0.5);
-    // Pair each participant with the next one 
-    const santasAssigned = shuffledParticipants.map((participant, index) => ({
+const assignSantas = (participants) => {
+  // Shuffle the participants array randomly
+  const shuffledParticipants = participants.sort(() => Math.random() - 0.5);
+  // Pair each participant with the next one and ensure the last participant is paired with the first one so all partecipants have a santa assigned
+  const santasAssigned = shuffledParticipants.map((participant, index) => ({
       participant,
       santa: shuffledParticipants[(index + 1) % shuffledParticipants.length],
       emoji: participant.emoji,
-    }));
-  
-    return santasAssigned;
-  }
+  }));
+
+  return santasAssigned;
+};
+
 
 
   return (
@@ -119,32 +169,72 @@ const handleSubmitUsername = () => {
               />
         </div>
         <div className='input-container'>
-          <h2>Your name</h2>
-          {username}
+          <h2>Your Details</h2>
+        {!username &&(<p className='expl'>
+              Enter your name and your email address
+          </p>)}
+         { username && (<p className='expl'> {username} - {organizerEmail}</p>)}
           {!username && (
-            <div className='input'>
-                <input
+             <div className='input'>
+                <div className='input-box'>
+                  <label>
+                    <FontAwesomeIcon icon={faUser}/>
+                  </label>
+                  <input
                     type="text"
                     placeholder="Enter your name"
-                    value={temporaryUsername} 
+                    value={temporaryUsername}
                     onChange={(e) => setTemporaryUsername(e.target.value)}
-                    />
-                <button onClick={handleSubmitUsername}>Submit</button>
-            </div>
+                  />
+                </div>
+             <div className='input-box'>
+                <label>
+                  <FontAwesomeIcon icon={faAt}/>
+                </label>
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  value={organizerEmail}
+                  onChange={(e) => setOrganizerEmail(e.target.value)}
+                />
+              </div>
+              <div className='submit-butt'>
+                  <button onClick={handleSubmitUsername}>Submit</button>
+              </div>
+           </div>
           )}
         </div>
         {username && (
           <>
             <div className='input-container'>
               <h2>Add Participants</h2>
+              <p className='expl'>Enter the partecipants, you will need to add an e-mail address of the user, in order for the Secret Santa to be sent to them.</p>
               <div className='input'>
+                <div className='input-box'>
+                  <label>
+                    <FontAwesomeIcon icon={faUser}/>
+                  </label>
                 <input
-                    type="text"
-                    placeholder="Enter participant's name"
-                    value={newParticipantName}
-                    onChange={(e) => setNewParticipantName(e.target.value)}
+                  type="text"
+                  placeholder="Enter participant's name"
+                  value={newParticipantName}
+                  onChange={(e) => setNewParticipantName(e.target.value)}
                 />
-                <button onClick={handleAddParticipant}>Add</button>
+                </div>
+                <div className='input-box'>
+                  <label>
+                    <FontAwesomeIcon icon={faAt}/>
+                  </label>
+                  <input
+                      type="email"
+                      placeholder="Enter participant's email"
+                      value={participantEmail}
+                      onChange={(e) => setParticipantEmail(e.target.value)}
+                    />
+                </div>
+                <div className='add-butt'>
+                  <button onClick={handleAddParticipant}>Add User</button>
+                </div>
               </div>
             </div>
           </>
@@ -156,7 +246,7 @@ const handleSubmitUsername = () => {
           <h3>({numParticipants}) Members</h3>
           <ul>
             {participants.map((participant, index) => (
-              <li key={index}>{participant.emoji} {participant.name}</li>
+              <li key={index}>{participant.emoji} {participant.name} <span className='email'>{participant.email}</span></li>
             ))}
           </ul>
       </div>
@@ -167,9 +257,9 @@ const handleSubmitUsername = () => {
      
           <div className='assigns'>
             <ul>
-              {assignments.map((assignment, index) => (
+              {participants.map((participant, index) => (
                 <li key={index}>
-                  {`${assignment.emoji} ${assignment.participant.name} is ${assignment.santa.name}'s Secret Santa`}
+                  {`${participant.emoji} ${participant.name} has been assigned`}
                 </li>
               ))}
             </ul>
