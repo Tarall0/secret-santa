f14cc38a742574e2899ea60cd76e1a603d779b16
Tarallo <98182571+Tarall0@users.noreply.github.com>
2023-12-04 19:20:44 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,15 +13,15 @@ const sendEmail = async (to, subject, body) => {
             port: 587,
             secure: false,
             auth: {
-                user: '',
-                pass: ''
+                user: 'email',
+                pass: 'passwordl'
             }
 
         })
 
         const email = {
-            from: '', // Sender email address
-            to, // Recipient email address
+            from,
+            to, 
             subject,
             html: body
           };
@@ -39,17 +39,28 @@ const sendEmail = async (to, subject, body) => {
 app.use(express.json());
 app.use(cors());
 
+
 // Helper function to get assigned Santa
 const getAssignedSanta = (participantName, participants) => {
-    const availableSantas = participants.filter(p => p.name !== participantName && !p.santa);
-    const assignedSanta = availableSantas[Math.floor(Math.random() * availableSantas.length)];
-    return assignedSanta ? assignedSanta : null;
-  };
+  // Create a copy of the participants array
+  const shuffledParticipants = [...participants].sort(() => Math.random() - 0.5);
+  // Get available Santas that have not been assigned yet
+  const availableSantas = shuffledParticipants.filter(p => p.name !== participantName && !p.assigned);
+  // Iterate over available Santas and find the assigned Santa
+  for (let i = 0; i < availableSantas.length; i++) {
+    const assignedSanta = availableSantas[(i + 1) % availableSantas.length];
+    // Mark the assigned Santa as "assigned" to prevent reassignment
+    assignedSanta.assigned = true;
+    return assignedSanta;
+  }
+  // If no assigned Santa is found, return null
+  return null;
+};
 
 app.post('/add-participants', (req, res) => {
     try {
       const { participants } = req.body;
-      const {organizer, date} = req.body;
+      const {organizer, date, groupname} = req.body;
           
       // Iterate through participants and send emails
       participants.forEach(({ name, email }) => {
@@ -57,17 +68,19 @@ app.post('/add-participants', (req, res) => {
   
         if (assignedSanta) {
           const subject = 'Secret Santa Assignment';
-          const body = `<h2 style="background: #d42834; color: #ffffff; padding: 1em; text-align: center; width: calc(100% - 2em); margin: 2em auto;"> 🎁 Secret Santa </h2>
+          const body = `<h2 style="background: #d42834; color: #ffffff !important; padding: 1em; text-align: center; width: calc(100% - 2em); margin: 2em auto;"> 🎁 Secret Santa </h2>
           <h1 style="text-align: center; width: 100%">❄️ 🎄🎅</h1>
-          <div style="max-width: 520px; margin: 1em auto;">
-            <div style="background: rgb(120, 120, 120, .10); padding: 1em; border-radius: 1em; width: calc(100% - 2em)">
+          <h2 style="text-align: center; width: 100%"> ${groupname}</h2>
+          <div style="max-width: 520px; margin: 1em">
+            <div style="background-color: #EEEEEE; padding: 1em; border-radius: 1em; width: calc(100% - 2em); border: 3px dotted #d42834;">
                 <h3>Dear ${name},</h3>
                 <p> Your Secret Santa is <b>${assignedSanta.name}</b>.</p>
                 <p style="padding: 1em; width: calc(100% - 2em); text-align: right; font-size: small;">- Secret Santa</p>
             </div>
+            <p style="font-size: small;">Organized by ${organizer} ${date}</p>
             <p> Make sure to contact the organizer for further details. Please note that the assignments should remain a secret.</p>
             <h4 style="text-align: center; width: 100%;">Happy Holidays!</h4>
-            <p style="font-size: small;">Organized by ${organizer} ${date}</p>
+            
           </div>
           <br>
           <br>
@@ -78,8 +91,9 @@ app.post('/add-participants', (req, res) => {
                 <span style="font-size: small;">Organize your own Secret Santa group</span>
           </p>`;
   
-          // Send email (using sendEmail function)
+          // Send email (using your sendEmail function)
           sendEmail(email, subject, body);
+          //console.log("Email sent to"+name+" to "+email+" - Assigned santa:" +assignedSanta.name)
         } else {
           console.error(`Error assigning Santa for participant: ${name}`);
         }
@@ -103,5 +117,5 @@ app.listen(PORT, () => {
 
 module.exports = {
     sendEmail
-  };
+};
   
